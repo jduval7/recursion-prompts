8ef66955b57d6915894aea46151290618d9b8a47
John Duval <john@rockitscienceagency.com>
2021-08-25 14:28:27 -0600

    "added logic for count values in nested obj"

--- src/recursion.js
+++ src/recursion.js
@@ -260,6 +260,19 @@ var countKeysInObj = function (obj, key) {
 // countValuesInObj(obj, 'r') // 2
 // countValuesInObj(obj, 'e') // 1
 var countValuesInObj = function (obj, value) {
+  var count = 0;
+
+  for (var key in obj ) {
+
+    if( typeof obj[key] === 'object') {
+      count = count + countValuesInObj(obj[key], value);
+    }
+
+    if(obj[key] === value ) {
+      return 1;
+    }
+  }
+  return count;
 };
 
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
